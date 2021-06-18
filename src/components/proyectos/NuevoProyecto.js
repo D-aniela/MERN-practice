43cf48571aa9f18dce261f99fcf9d6097ec4cbd7
D-aniela <daniela.es.ib@gmail.com>
2021-06-17 21:37:37 -0500

    on submit de proyecto

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -9,6 +9,7 @@ const NuevoProyecto = () => {
   //extraer nombre de proyecto
   const { nombre } = proyecto
 
+  //lees los contenidos del input
   const onChangeProyecto = e => {
     guardarProyecto({
       ...proyecto,
@@ -16,12 +17,17 @@ const NuevoProyecto = () => {
     })
   }
 
+  //cuando el usuario envia un proyecto
+  const onSubmitProyecto = e => {
+    e.preventDefault()
+  }
+  
   return (
     <Fragment>
       <button type="button" className="btn btn-block btn-primario">
         Nuevo Proyecto
       </button>
-      <form className="formulario-nuevo-proyecto">
+      <form className="formulario-nuevo-proyecto" onSubmit={onSubmitProyecto}>
         <input
           type="text"
           className="input-text"
