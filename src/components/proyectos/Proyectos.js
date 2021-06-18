f9d137793523597c0484868cf5d5ee6f941fa404
D-aniela <daniela.es.ib@gmail.com>
2021-06-17 20:05:11 -0500

    import sidebar

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -1,20 +1,17 @@
 import React from 'react'
+import Sidebar from '../layout/Sidebar'
 
 const Proyectos = () => {
   return (
-  <div className="contenedor-app">
-    <aside>
+    <div className="contenedor-app">
+      <Sidebar />
 
-    </aside>
-
-    <div className="seccion-principal">
-      <main>
-        <div className="contenedor-tareas">
-          
-        </div>
-      </main>
+      <div className="seccion-principal">
+        <main>
+          <div className="contenedor-tareas"></div>
+        </main>
+      </div>
     </div>
-  </div>
   )
 }
 
