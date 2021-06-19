f18dc67cb35e1c1be1ce86f0def502975a844310
D-aniela <daniela.es.ib@gmail.com>
2021-06-19 00:38:53 -0500

    crear componente listado

---
+++ src/components/proyectos/Listado.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Proyecto from './Proyecto'
+
+const ListadoProyectos = () => {
+  return <ul className="listado-proyectos"></ul>
+}
+
+export default ListadoProyectos
