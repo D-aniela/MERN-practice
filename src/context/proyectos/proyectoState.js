758b1f336a53078d6cc59c344d211e55308c5371
D-aniela <daniela.es.ib@gmail.com>
2021-06-19 19:49:07 -0500

    create context, creat state de form

---
+++ src/context/proyectos/proyectoState.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import proyectoContext from './proyectoContext'
+
+const proyectoState = props => {
+  //pasar state inicial
+  const initialState = {
+    nuevoProyecto: false
+  }
+}
