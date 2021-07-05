aae49ddc1ddf0580522f69ad2eb9fff38f2d3ec1
D-aniela <daniela.es.ib@gmail.com>
2021-07-04 21:22:40 -0500

    creando controladores

--- servidor/index.js
+++ servidor/index.js
@@ -7,6 +7,9 @@ const app = express()
 // Conectar a la base de datos
 conectarDB()
 
+// Importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'))
+
 // puerto de la app
 const PORT = process.env.PORT || 4000
 
