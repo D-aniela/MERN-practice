9651b5fe87e0f9f1551f0066c40dc4486834aa67
D-aniela <daniela.es.ib@gmail.com>
2021-07-04 22:40:55 -0500

    configuracion de modelo y controllador de usuario

--- servidor/controllers/usuariocontroller.js
+++ servidor/controllers/usuariocontroller.js
@@ -1,3 +1,26 @@
-exports.crearUsuario = (req, res) => {
-  console.log(req.body)
+const Usuario = require('../models/Usuario')
+
+exports.crearUsuario = async (req, res) => {
+  // extraer email y password
+  const { email, password } = req.body
+
+  try {
+    // Revisar que el usuario registrado sea único
+    let usuario = await Usuario.findOne({ email })
+    if (usuario) {
+      return res.status(400).json({ msg: 'El usuario ya existe' })
+    }
+
+    // crea el nuevo usuario
+    usuario = new Usuario(req.body)
+
+    // guardar usuario
+    await usuario.save()
+
+    // Mensaje de confirmación
+    res.json({ msg: 'Usuario creado correctamente' })
+  } catch (error) {
+    console.log(error)
+    res.status(400).send('Hubo un error')
+  }
 }
